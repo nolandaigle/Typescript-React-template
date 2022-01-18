03788b5af9a4da2f98785ccb05f400e476babe10
nolandaigle <nolan.daigle@gmail.com>
2022-01-18 15:52:23 -0800

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Test from './components/test'
+import UserComponent from './components/UserComponent'
 
 ReactDOM.render(
     <div>
-    <h1>Test</h1>
-    <Test/>
+        <div className="title">
+            <h1 >Test</h1>
+        </div>
+    <UserComponent name="Nolan" age={26} address="blah blah blah" dob={new Date()} />
     </div>,
     document.getElementById('app-root'),
 )
